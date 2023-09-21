fa07afd17a3376f81a46d1309a651b538ab1d1bd
Iamneo <techadmin@iamneo.ai>
2023-09-21 10:58:28 +0000

    Backup Commit

--- qu1/qu1.tf
+++ qu1/qu1.tf
@@ -7,6 +7,16 @@ terraform {
     }
 } 
 provider "aws" {
-    region = ""
-    access_key = "" 
+    region = "ap-northeast-1"
+    access_key = "AKIA2NMMEGGPJP5ZLD7T" 
+    secret_key = "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" 
+} 
+
+resource "aws_instance" "linux_server" {
+    ami = "ami-0d52744d6551d851e"
+    instance_type = "t2.micro"
+} 
+
+output "public_ip" {
+    value = aws_instance.linux_server.public_ip 
 }
